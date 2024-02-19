3a5dc1305f5b4f68a29b20ab7f2df6f230b49b44
Monica Garcia <monicairenegarcia@gmail.com>
2024-02-19 08:43:46 -0700

    working pointsForWord function

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,15 @@
-// Your code here
+export function pointsForWord(word) {
+    let sumTotal = 0;
+
+    const wordArray = word.split('');
+    console.log(wordArray)
+    wordArray.forEach((letter) => {
+        if (letter.toLowerCase() !== "a" && letter.toLowerCase() !== "e" && letter.toLowerCase() !== "i" && letter.toLowerCase() !== "o" && letter.toLowerCase() !== "u") {
+            sumTotal += 2
+        }
+        else sumTotal += 1;
+    })
+    return sumTotal;
+}
+
+
