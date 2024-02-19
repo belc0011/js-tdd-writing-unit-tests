7a0953831c27dedf8d089b46170fb229e21b462f
Monica Garcia <monicairenegarcia@gmail.com>
2024-02-19 08:55:00 -0700

    final pointsForWord refactor

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -2,7 +2,7 @@ import { pointsForWord } from "../utils.js";
 
 describe("pointsForWord", () => {
     it("calculates the total points for a word (1 point per vowel, 2 per consontant", () => {
-        const word = "test";
+        const word = "tEsT";
 
         const points = pointsForWord(word);
 
