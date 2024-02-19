47f8c1d5726ac87af5d06514e5f1bb4a5522ad02
Monica Garcia <monicairenegarcia@gmail.com>
2024-02-19 08:52:30 -0700

    refactor pointsForWord function

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,9 @@
 export function pointsForWord(word) {
-    let sumTotal = 0;
-
-    const wordArray = word.split('');
-    console.log(wordArray)
-    wordArray.forEach((letter) => {
-        if (letter.toLowerCase() !== "a" && letter.toLowerCase() !== "e" && letter.toLowerCase() !== "i" && letter.toLowerCase() !== "o" && letter.toLowerCase() !== "u") {
-            sumTotal += 2
-        }
-        else sumTotal += 1;
-    })
-    return sumTotal;
-}
+    let points = 0;
+    for (const char of word) {
+      points += /[aeiou]/.test(char) ? 1 : 2;
+    }
+    return points;
+  }
 
 
